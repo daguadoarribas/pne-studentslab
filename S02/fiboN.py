8517d39bfc18ff457975d77ea7cb52890e2c9c4c
daguadoarribas <d.aguadoa.2023@alumnos.urjc.es>
2024-01-30 16:25:40 +0100

    This is going to be my first but not last commit

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -8,10 +8,12 @@ def fibon(n):
     return x
 
 term_5 = fibon(5)
-term_10 = fibon(10)
-term_15 = fibon(15)
-
 print(f"The 5th term is: {term_5}")
+term_10 = fibon(10)
 print(f"The 10th term is: {term_10}")
+term_15 = fibon(15)
 print(f"The 15th term is: {term_15}")
+
+
+
 
