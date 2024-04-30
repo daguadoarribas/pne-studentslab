c5195bf234902c065d80435403e7bc5157df7890
daguadoarribas <d.aguadoa.2023@alumnos.urjc.es>
2024-04-30 18:20:40 +0200

    This is going to be my first but not last commit

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -4,8 +4,8 @@ import termcolor
 from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 import jinja2 as j
-from Seq1 import Seq
-# -- Server network parameters
+from P01.Seq1 import Seq
+
 PORT = 8080
 
 sequences = ["ACGT", "ACCCGGTA", "TACATG", "ACACG", "ACCT"]
@@ -39,10 +39,10 @@ def for_gene(arguments):
 
 def info_function(s):
     result = f"Total length: {s.len()}"
-    result += f"<br><br>A:{s.count_base('A')} ({s.count_base('A') / s.len() * 100}%)"
-    result += f"<br><br>C:{s.count_base('C')} ({s.count_base('C') / s.len() * 100}%)"
-    result += f"<br><br>G:{s.count_base('G')} ({s.count_base('G') / s.len() * 100}%)"
-    result += f"<br><br>T:{s.count_base('T')} ({s.count_base('T') / s.len() * 100}%)"
+    result += f"<br><br>A:{s.seq_count_base('A')} ({s.seq_count_base('A') / s.len() * 100}%)"
+    result += f"<br><br>C:{s.seq_count_base('C')} ({s.seq_count_base('C') / s.len() * 100}%)"
+    result += f"<br><br>G:{s.seq_count_base('G')} ({s.seq_count_base('G') / s.len() * 100}%)"
+    result += f"<br><br>T:{s.seq_count_base('T')} ({s.seq_count_base('T') / s.len() * 100}%)"
     return result
 
 
@@ -54,9 +54,9 @@ def for_operation(arguments):
     if operation == "info":
         result = info_function(s)
     elif operation == "comp":
-        result = s.complement()
+        result = s.seq_complement()
     else:
-        result = s.reverse()
+        result = s.seq_reverse()
     context = {'seq': seq, 'operation': operation, 'result': result}
     contents = contents.render(context=context)
     return contents
@@ -87,33 +87,20 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = Path("html/error.html").read_text()
             self.send_response(404)
 
-
-        # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
-
-        # The header is finished
         self.end_headers()
-
-        # Send the response message
         self.wfile.write(contents.encode())
 
         return
 
-# ------------------------
-# - Server MAIN program
+
 socketserver.TCPServer.allow_reuse_address = True
-# ------------------------
-# -- Set the new handler
 Handler = TestHandler
 
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-
     print("Serving at PORT", PORT)
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
