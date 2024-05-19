5533dd17f2151c80f9e1b962e291744316bc80d3
daguadoarribas <d.aguadoa.2023@alumnos.urjc.es>
2024-05-19 22:23:15 +0200

    This is going to be my first but not last commit

--- Final-Project/class_seq.py
+++ Final-Project/class_seq.py
@@ -1,27 +1,28 @@
-def valid_bases(strbases):
+def accepted_bases(strbases):
     valid = True
-    for b in strbases:
-        if b not in Seq.bases:  # the Seq in front indicates that bases_list is a class attribute
+    for i in strbases:
+        if i not in Seq.bases:
             valid = False
-            break  # you exit the for loop, but not the entire function
+            break
     return valid
 
 
 class Seq:
-    """A class for representing sequences"""
+
     bases = ["A", "C", "T", "G"]
     complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
 
     def __init__(self, strbases=None):
-        if strbases is None or len(strbases) == 0:  # the same as strbases == 0 --> empty
+        if strbases is None or len(strbases) == 0:
             self.strbases = "NULL"
             print("NULL sequence created")
-        elif valid_bases(strbases):  # if the seq/strbases contains the same bases as in BASES, then it is VALID
+        elif accepted_bases(strbases):
             self.strbases = strbases
             print("New sequence created!")
-        else:  # if it is not VALID, and it is not NULL, then it is INCORRECT/INVALID
+        else:
             self.strbases = "ERROR"
             print("INVALID sequence!")
+
 
     def __str__(self):
         return self.strbases
@@ -29,8 +30,6 @@ class Seq:
     def len(self):
         if self.strbases == "NULL" or self.strbases == "ERROR":
             length = 0
-            # return 0
-        # return len(self.strbases)
         else:
             length = len(self.strbases)
         return length
@@ -38,18 +37,15 @@ class Seq:
     def count_base(self, base):
         if self.strbases == "NULL" or self.strbases == "ERROR":
             count = 0
-            # return 0
-        # return self.strbases.count(base)
         else:
             count = self.strbases.count(base)
         return count
 
-    def count(self):  # as we use count_base, in this function we don't need to check if the sequence is valid
-        bases_dict = {}
-
-        for b in Seq.bases:  # b acting as each one of the bases of our list of valid bases(class property BASES)
-            bases_dict[b] = self.count_base(b)  # the value of each one of the keys(b) will be their individual count
-        return bases_dict
+    def count(self):
+        dict_bases = {}
+        for i in Seq.bases:
+            dict_bases[i] = self.count_base(i)
+        return dict_bases
 
     def reverse(self):
         if self.strbases == "NULL":
@@ -62,25 +58,23 @@ class Seq:
 
     def complement(self):
         if self.strbases == "NULL":
-            complementary_seq = "NULL"
+            complement_seq = "NULL"
         elif self.strbases == "ERROR":
-            complementary_seq = "ERROR"
+            complement_seq = "ERROR"
         else:
-            complementary_seq = ""
-            for b in self.strbases:
-                complementary_seq += Seq.complements[b]
-        return complementary_seq
+            complement_seq = ""
+            for i in self.strbases:
+                complement_seq += Seq.complements[i]
+        return complement_seq
 
     def read_fasta(self, filename):
         from pathlib import Path
-
         file_content = Path(filename).read_text()
         lines = file_content.splitlines()
         body = lines[1:]
-        # "".join(body) is the same as the following for loop
         self.strbases = ""
         for line in body:
-            self.strbases += line  # dna_sequence = dna_sequence + line
+            self.strbases += line
 
     def max_base(self):
         if self.strbases == "NULL" or self.strbases == "ERROR":
@@ -95,12 +89,11 @@ class Seq:
         return max_base
 
     def info(self):
-        s = f"Total length: {self.len()}\n"
+        i = f"Total length: {self.len()}\n"
         for base in Seq.bases:
-            if self.len() == 0:  # we discard the invalid and null sequences
+            if self.len() == 0:
                 percentage = 0
             else:
                 percentage = round((self.count_base(base) * 100) / self.len(), 1)
-            s += f"{base}: {self.count_base(base)} ({percentage}%)\n"
-
-        return s
+            i += f"{base}: {self.count_base(base)} ({percentage}%)\n"
+        return i
